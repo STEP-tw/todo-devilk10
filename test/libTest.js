2325c9b91fe9a0e1a1d2e79001aa2b1fec8dde56
devilk10 <ksangle24@gmail.com>
2018-01-17 17:31:58 +0530

    Unique Id given to each todo and its item

--- test/libTest.js
+++ test/libTest.js
@@ -5,7 +5,7 @@ let assert = require('chai').assert;
 
 describe('todo',()=>{
   beforeEach(()=>{
-    todoList=new Todo('Title','detail');
+    todoList=new Todo('Title','detail',0);
   });
 
   it('Should create todo of Title:"title"',()=>{
@@ -14,27 +14,24 @@ describe('todo',()=>{
   });
 
   it('addItem should add item to todo list',()=>{
-    let item = new Task('Type something');
-    todoList.addItem(item);
-    assert.deepEqual(todoList.task,{"Type something": {"_isDone": false,
-      "caption": "Type something"}
-    });
+    todoList.addItem('Type something');
+    let expected={ '0': {
+      caption: 'Type something', _isDone: false }}
+    assert.deepEqual(todoList.task,expected);
   });
 
   it('getItem should give list of todo task',()=>{
-    let item = new Task('Type something');
-    todoList.addItem(item);
+    todoList.addItem('Type something');
     let tasks=todoList.getItems();
-    let expected={"Type something": {"_isDone": false,
+    let expected={0: {"_isDone": false,
       "caption": "Type something"}};
     assert.deepEqual(tasks,expected);
   })
 
   it('editItem should delete given item',()=>{
-    let item = new Task('Type something');
-    todoList.addItem(item);
-    todoList.editItem('Type something','Changed task');
-    assert.deepEqual(todoList.getItems(),{"Changed task": {"_isDone": false,
+    todoList.addItem('Type something');
+    todoList.editItem(0,'Changed task');
+    assert.deepEqual(todoList.getItems(),{1: {"_isDone": false,
       "caption": "Changed task"}
     });
   })
@@ -45,16 +42,14 @@ describe('todo',()=>{
   })
 
   it('checkIsDone gives false if status is false',()=>{
-    let item = new Task('Type something');
-    todoList.addItem(item);
-    assert.notOk(todoList.checkIsDone(item.caption));
+    todoList.addItem('Type something');
+    assert.notOk(todoList.checkIsDone(0));
   })
 
   it('checkIsDone gives true task is completed',()=>{
-    let item = new Task('Type something');
-    todoList.addItem(item);
-    todoList.markTaskDone(item.caption);
-    assert.ok(todoList.checkIsDone(item.caption));
+    todoList.addItem('Type something');
+    todoList.markTaskDone(0);
+    assert.ok(todoList.checkIsDone(0));
   })
 })
 
@@ -94,7 +89,7 @@ describe('User',()=>{
 
   it('getTodos should give all todos ',()=>{
     user.createTodo('This is title','This is description');
-    let expected = {"This is title": {"title": "This is title","description": "This is description","task": {}}}
+    let expected = {"0": {"title": "This is title","description": "This is description","id": 0,"task": {},"taskTag":0}}
     assert.deepEqual(user.getTodos(),expected);
   })
 
@@ -110,52 +105,52 @@ describe('User',()=>{
 
   it('getTodoItems should give task list',()=>{
     user.createTodo('Todo','This is description');
-    user.addTodoItem('Todo','this is task');
-    let input = user.getTodoItems('Todo');
-    let expected = { 'this is task': { caption: 'this is task', _isDone: false } }
+    user.addTodoItem(0,'this is task');
+    let input = user.getTodoItems(0);
+    let expected = { 0: { caption: 'this is task', _isDone: false } }
     assert.deepEqual(input,expected);
   })
 
   it('getTodoItems should give empty list if todo does not exist',()=>{
-    assert.deepEqual(user.getTodoItems('Todo'),[]);
+    assert.deepEqual(user.getTodoItems(1),[]);
   })
 
   it('getTodoItems should give empty object if tasks are not entered',()=>{
     user.createTodo('Todo','This is description');
-    assert.deepEqual(user.getTodoItems('Todo'),{});
+    assert.deepEqual(user.getTodoItems(0),{});
   })
 
   it('addTodoItem should add todo in todo object',()=>{
     user.createTodo('Todo','This is description');
-    user.addTodoItem('Todo','this is task');
-    assert.notDeepEqual(user.getTodoItems('Todo'),{});
+    user.addTodoItem(0,'this is task');
+    assert.notDeepEqual(user.getTodoItems(0),{});
   })
 
   it('removeTodoItem should remove the given task',()=>{
     user.createTodo('Todo','This is description');
-    user.addTodoItem('Todo','this is task');
-    user.removeTodoItem('Todo','this is task');
-    assert.deepEqual(user.getTodoItems('Todo'),{});
+    user.addTodoItem(0,'this is task');
+    user.removeTodoItem(0,0);
+    assert.deepEqual(user.getTodoItems(0),{});
   })
 
   it('editTodoItem should edit task text',()=>{
     user.createTodo('Todo','This is description');
-    user.addTodoItem('Todo','this is task');
-    user.editTodoItem('Todo','this is task','same task');
-    let expected={"same task": {"caption":"same task","_isDone":false}};
-    assert.deepEqual(user.getTodoItems('Todo'),expected);
+    user.addTodoItem(0,'this is task');
+    user.editTodoItem(0,0,'same task with diff text');
+    let expected={1: {"caption":"same task with diff text","_isDone":false}};
+    assert.deepEqual(user.getTodoItems(0),expected);
   })
 
   it('deleteTodo should delete the given todo',()=>{
     user.createTodo('Todo','This is description');
-    user.deleteTodo('Todo');
+    user.deleteTodo(0);
     assert.deepEqual(user.getTodoTitles(),[]);
   })
 
   it('editTodoTitle should edit todo title',()=>{
     user.createTodo('Todo','This is description');
-    user.editTodoTitle('Todo','This is todo');
-    let expected={"This is todo": {"title": "This is todo","description": "This is description","task": {}}}
+    user.editTodoTitle(0,'This is todo');
+    let expected={1: {"title": "This is todo","description": "This is description","id": 1,"taskTag":0,"task": {}}}
     assert.deepEqual(user.getTodos(),expected);
   })
 })
